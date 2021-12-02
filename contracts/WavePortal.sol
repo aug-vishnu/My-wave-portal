a105b3af0c84e78bb4a4ff99bb9739291edbab31
Vishnu <vishnu@zoconut.com>
2021-12-02 21:04:07 +0530

    First log from contract

---
+++ contracts/WavePortal.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract WavePortal {
+    constructor() {
+        console.log("Yo yo, I am a contract and I am smart ! - Hello nodes");
+    }
+}
